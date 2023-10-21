01ded6c70c89837b6f4e9eacb741a8983c23b54a
yoshikouki <yoshikouki@gmail.com>
2023-10-21 13:31:27 +0900

    Create Editor story

---
+++ src/components/Editor/Editor.stories.tsx
@@ -0,0 +1,23 @@
+import Editor from "./Editor";
+
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Components/Editor",
+  component: Editor,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+  },
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Default = {
+  args: {
+  },
+};
