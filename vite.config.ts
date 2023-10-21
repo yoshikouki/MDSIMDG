dbe73e8551acf34b693c69bd5e696c03ffac263a
yoshikouki <yoshikouki@gmail.com>
2023-10-21 13:50:10 +0900

    Build with Vite

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: "src/index.tsx",
+      name: "MDSIMDG",
+      fileName: (format) => `mdsimdg.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
