6d87b3c2b36ec3aee2f790e7a47b726a8e6ac4ff
yoshikouki <yoshikouki@gmail.com>
2023-10-26 23:33:49 +0900

    Add Floating Toolbar

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -3,19 +3,23 @@
 import { AutoFocusPlugin } from "@lexical/react/LexicalAutoFocusPlugin";
 import { CheckListPlugin } from "@lexical/react/LexicalCheckListPlugin";
 import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
 import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
 import { ListPlugin } from "@lexical/react/LexicalListPlugin";
 import { RichTextPlugin as LexicalRichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import { TabIndentationPlugin } from "@lexical/react/LexicalTabIndentationPlugin";
 import { TablePlugin } from "@lexical/react/LexicalTablePlugin";
 import { UpdateListener } from "lexical/LexicalEditor";
-import React, { ComponentProps } from "react";
+import React, { ComponentProps, useState } from "react";
+import Placeholder from "../Placeholder";
 import "./Editor.css";
 import { EditorProvider } from "./EditorProvider";
 import AutoLinkPlugin from "./plugins/AutoLinkPlugin";
 import CodeHighlightPlugin from "./plugins/CodeHighlightPlugin";
 import MarkdownShortcutPlugin from "./plugins/MarkdownShortcutPlugin";
 import { OnChangePlugin } from "./plugins/OnChangePlugin";
-import { RichTextPlugin } from "./plugins/RichTextPlugin";
+import FloatingTextFormatToolbarPlugin from "./plugins/FloatingTextFormatToolbarPlugin";
 
 export type InitialConfig = ComponentProps<
   typeof LexicalComposer
@@ -35,13 +39,35 @@ const Editor: React.FC = ({
   initialConfig,
   richTextPluginProps,
 }: EditorProps) => {
+  const [floatingAnchorElem, setFloatingAnchorElem] =
+    useState<HTMLDivElement | null>(null);
+  const onRef = (_floatingAnchorElem: HTMLDivElement) => {
+    if (_floatingAnchorElem !== null) {
+      setFloatingAnchorElem(_floatingAnchorElem);
+    }
+  };
+
   return (
     <EditorProvider initialConfig={initialConfig}>
       <div className="editor-shell">
         <div className="editor-container">
-          <RichTextPlugin {...richTextPluginProps} />
+          <LexicalRichTextPlugin
+            contentEditable={
+              <div className="editor-scroller">
+                <div className="editor" ref={onRef}>
+                  <ContentEditable />
+                </div>
+              </div>
+            }
+            placeholder={
+              <Placeholder>心に残っていることはありますか？</Placeholder>
+            }
+            ErrorBoundary={LexicalErrorBoundary}
+            {...richTextPluginProps}
+          />
           <HistoryPlugin />
           <AutoFocusPlugin />
+          <TabIndentationPlugin />
           <OnChangePlugin />
 
           <MarkdownShortcutPlugin />
@@ -50,6 +76,13 @@ const Editor: React.FC = ({
           <ListPlugin />
           <CheckListPlugin />
           <TablePlugin hasCellMerge={true} hasCellBackgroundColor={false} />
+          {floatingAnchorElem && (
+            <>
+              <FloatingTextFormatToolbarPlugin
+                anchorElem={floatingAnchorElem}
+              />
+            </>
+          )}
         </div>
       </div>
     </EditorProvider>
